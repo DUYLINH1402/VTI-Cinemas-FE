34cae582d50d48a3ba86c67370f39f2b0779d6b4
damphu1102 <129761709+damphu1102@users.noreply.github.com>
2024-11-12 22:16:32 +0700

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,17 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-<<<<<<< HEAD
 import { Home } from "../pages/Home/Home";
 import { ErrorPage } from "../pages/Error/ErrorPage";
 import { Layout } from "./../components/Layout/Layout";
 import { Movies } from "../pages/Movies/Movies";
-import { Cinemas } from "../pages/Cinema/Cinemas";
-=======
-// import { Home } from "./Home";
-// import { ErrorPage } from "../Error/ErrorPage";
-// import { Layout } from "./Layout";
-// import { Movies } from "../Movies/Movies";
-// import { Cinemas } from "../Cinemas/Cinemas";
-// >>>>>>> 5a10aeeb4ddfd4c99fdecda228ecd887a8e41f4c
+import { Cinemas } from "../pages/Cinemas/Cinemas";
+import { MovieInf } from "../pages/Movies/MovieInf/MovieInf";
 
 // Create react router dom
 const routerPage = createBrowserRouter([

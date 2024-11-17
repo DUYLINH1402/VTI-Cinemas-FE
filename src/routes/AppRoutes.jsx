41959c15d4f9ec91ad1ded09030c23bd68f32440
DUYLINH1402 <duylinh63b5@gmail.com>
2024-11-17 12:35:03 +0900

    Fixed CONFLIC

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,11 +5,8 @@ import { Layout } from "./../components/Layout/Layout";
 import { Movies } from "../pages/Movies/Movies";
 import { Cinemas } from "../pages/Cinemas/Cinemas";
 import { MovieInf } from "../pages/Movies/MovieInf/MovieInf";
-<<<<<<< HEAD
 import Members from "../pages/Members/Members";
-=======
 import { Booking_Seat } from "../pages/Booking_Seat/Booking_Seat";
->>>>>>> f57af03eb593efcb337fa5081d57140861b2f780
 
 // Create react router dom
 const routerPage = createBrowserRouter([

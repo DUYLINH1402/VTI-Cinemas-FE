dcbbfd6c6cc33fbdeea5790a99ab653cf3394215
tzanlam <tzanlam@gmail.com>
2024-11-22 20:59:14 +0700

    code

---
+++ src/constant/enumExport.js
@@ -0,0 +1,100 @@
+export const GENDER_OPTIONS = [
+    {value: "MALE", label: "Male"},
+    {value: "FEMALE", label: "Female"}
+  ] //1
+export const GENRE_OPTIONS = [
+    { value: "ACTION", label: "Action" },
+    { value: "DRAMA", label: "Drama" },
+    { value: "COMEDY", label: "Comedy" },
+    { value: "ROMANCE", label: "Romance" },
+    { value: "HORROR", label: "Horror" },
+    { value: "SCI_FI", label: "Sci-Fi" },
+  ]; //2
+export const LANGUAGE_OPTIONS = [
+    { value: "VIETNAMESE", label: "Vietnamese" },
+    { value: "ENGLISH", label: "English" },
+    { value: "JAPANESE", label: "Japanese" },
+    { value: "INDIA", label: "India" },
+  ];//3
+  export const SCREEN_TYPE_OPTIONS = [
+    { value: "IMAX", label: "IMAX" },
+    { value: "NORMAL", label: "Normal" },
+  ];//4 
+  export const STATUS_ACCOUNT_OPTIONS = [
+    { value: "ACTIVE", label: "Active" },
+    { value: "BLOCK", label: "Block" },
+    { value: "INACTIVE", label: "Inactive" },
+    { value: "PENDING", label: "Pending" },
+  ];// 5
+  export const STATUS_BOOKING_OPTIONS = [
+    { value: "SUCCESS", label: "Success" },
+    { value: "PENDING", label: "Pending" },
+    { value: "CANCELLED", label: "Cancelled" },
+  ];//6
+  export const STATUS_CINEMA_OPTIONS = [
+    { value: "OPEN", label: "Open" },
+    { value: "CLOSED", label: "Closed" },
+    { value: "BUILDING", label: "Building" },
+  ];// 7
+  export const STATUS_MOVIE_OPTIONS = [
+    { value: "COMING_SOON", label: "Coming Soon" },
+    { value: "SHOWING", label: "Showing" },
+    { value: "SPECIAL", label: "Special" },
+    { value: "CLOSE", label: "Close" },
+  ];// 8
+  export const STATUS_PAYMENT_OPTIONS = [
+    { value: "ACTIVE", label: "Active" },
+    { value: "INACTIVE", label: "Inactive" },
+  ];// 9
+  export const STATUS_RECEIPT_OPTIONS = [
+    { value: "PROCESSED", label: "Processed" },
+    { value: "UNPROCESSED", label: "Unprocessed" },
+  ];//10
+  export const STATUS_ROOM_OPTIONS = [
+    { value: "AVAILABLE", label: "Available" },
+    { value: "MAINTENANCE", label: "Maintenance" },
+    { value: "BOOKED", label: "Booked" },
+  ];//11
+  export const STATUS_SEAT_ROOM_OPTIONS = [
+    { value: "AVAILABLE", label: "Available" },
+    { value: "OCCUPIED", label: "Occupied" },
+    { value: "BOOKED", label: "Booked" },
+  ];//12
+  export const STATUS_SERVICE_OPTIONS = [
+    { value: "ACTIVE", label: "Active" },
+    { value: "INACTIVE", label: "Inactive" },
+    { value: "AVAILABLE", label: "Available" },
+    { value: "OUT_OF_STOCK", label: "Out of Stock" },
+  ];//13
+  export const STATUS_TICKET_OPTIONS = [
+    { value: "PAID", label: "Paid" },
+    { value: "UNPAID", label: "Unpaid" },
+    { value: "CANCELLED", label: "Cancelled" },
+  ];//14
+  export const STATUS_VOUCHER_OPTIONS = [
+    { value: "EFFECTIVE", label: "Effective" },
+    { value: "EXPIRED", label: "Expired" },
+  ];//15
+  export const TYPE_PAYMENT_OPTIONS = [
+    { value: "BANKING", label: "Banking" },
+    { value: "E_WALLET", label: "E-Wallet" },
+  ]; // 16  
+  export const TYPE_RECEIPT_OPTIONS = [
+    { value: "INCOME", label: "Income" },
+    { value: "SPENDING", label: "Spending" },
+  ];//17
+  export const TYPE_SEAT_OPTIONS = [
+    { value: "SINGLE", label: "Single", price: 100 },
+    { value: "VIP", label: "VIP", price: 150 },
+    { value: "DOUBLE", label: "Double", price: 200 },
+  ];//18
+  export const VIEWING_AGE_OPTIONS = [
+    { value: "CHILD", label: "Child" },
+    { value: "T15", label: "15+" },
+    { value: "T18", label: "18+" },
+    { value: "PG13", label: "PG-13" },
+    { value: "R", label: "Restricted (R)" },
+    { value: "G", label: "General (G)" },
+    { value: "PG", label: "Parental Guidance (PG)" },
+  ];//19
+                  
